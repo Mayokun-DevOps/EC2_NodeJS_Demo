eddfd52e24822a31bf036e6e54029c6887159f29
Mayokun-DevOps <sogsax@gmail.com>
2025-08-25 20:47:25 +0100

    Updating index.js File

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hey Guys, Welcome to Node_JS App deployed with Docker on AWS EC2!\n');
+  res.end('Hey Guys, Welcome to Node_JS App deployed with Docker on AWS EC2!!\n');
 });
 
 server.listen(port, () => {
